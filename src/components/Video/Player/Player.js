cf58fcf682d42f638a30ef8445de02829483f160
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-08-10 10:00:17 +0300

    add Player component

--- src/components/Video/Player/Player.js
+++ src/components/Video/Player/Player.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import classes from './Player.module.css';
 
 const player = (props) => {
-  return <section>Player</section>;
+  return (
+    <section className={classes.Player}>
+      <div>
+        <iframe
+          title="formula 1"
+          src="https://www.youtube.com/embed/ytLAOiYnNtU"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </section>
+  );
 };
 
 export default player;
