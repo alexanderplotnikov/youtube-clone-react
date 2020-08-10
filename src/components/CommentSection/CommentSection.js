609a867d8cc5f078099ade5115ebb1363e35395c
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-08-10 13:37:42 +0300

    add Comments component

--- src/components/CommentSection/CommentSection.js
+++ src/components/CommentSection/CommentSection.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import AddComment from './AddComment/AddComment';
 import Comments from './Comments/Comments';
+import classes from './CommentSection.module.css';
 
 const commentSection = (props) => {
   return (
-    <article>
-      <AddComment />
+    <article className={classes.CommentSection}>
+      <div style={{ display: 'flex', marginBottom: '24px', marginTop: '16px' }}>
+        <p>2,014 Comments</p>
+        <button>Sort By</button>
+      </div>
+      <div className={classes.CommentInfo}>
+        <AddComment comment />
+      </div>
+
       <Comments />
     </article>
   );
--- src/components/CommentSection/Comments/Comments.js
+++ src/components/CommentSection/Comments/Comments.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import AddComment from '../AddComment/AddComment';
+import classes from './Comments.module.css';
 
 const comments = (props) => {
-  return <div>comments</div>;
+  const commentsList = [{ message: 'first comment' }];
+
+  return (
+    <article className={classes.Comment}>
+      <img alt="avatar" />
+      <div className={classes.CommentBody}>
+        <div className={classes.CommentHeader}>
+          <p>Name</p>
+          <span>6 days ago</span>
+        </div>
+        <p>First comment</p>
+        <div className={classes.CommentControls}>
+          <button>Like</button>
+          <p>12</p>
+          <button>Disike</button>
+          <button>Reply</button>
+        </div>
+        <div>
+          <AddComment />
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default comments;
