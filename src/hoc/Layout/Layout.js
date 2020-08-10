b2dbcde46132862036a0a1600ca0f16ba0ea7c28
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-08-10 19:06:21 +0300

    finish layout

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Toolbar from '../../components/Toolbar/Toolbar';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import Backdrop from '../../components/UI/Backdrop/Backdrop';
 import classes from './Layout.module.css';
+import disableScroll from '../../helpers/disableScroll';
+import enableScroll from '../../helpers/enableScroll';
 
-const layout = (props) => (
-  <div className={classes.Layout}>
-    {/* Toolbar, Sidebar, Backdrop */}
-    <main className={classes.Content}>{props.children}</main>
-  </div>
-);
+const Layout = (props) => {
+  const [show, setShow] = useState(false);
+  const handleToggleSidebar = () => {
+    const state = !show;
+    setShow(state);
+    if (!show) {
+      disableScroll();
+    } else {
+      enableScroll();
+    }
+  };
+  return (
+    <div className={classes.Layout}>
+      <Sidebar show={show} clicked={handleToggleSidebar} />
+      <Toolbar clicked={handleToggleSidebar} />
+      <Backdrop show={show} clicked={handleToggleSidebar} />
+      <main className={classes.Content}>{props.children}</main>
+    </div>
+  );
+};
 
-export default layout;
+export default Layout;
