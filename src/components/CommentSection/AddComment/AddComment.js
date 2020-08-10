9902f176574a2c79c6b7eb7e1a92e914e20a8eee
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-08-10 13:38:10 +0300

    add AddComment component

--- src/components/CommentSection/AddComment/AddComment.js
+++ src/components/CommentSection/AddComment/AddComment.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classes from './AddComment.module.css';
 
-const addComment = (props) => {
-  return <section>add comment</section>;
+const AddComment = (props) => {
+  const [show, setShow] = useState(false);
+  console.log(props.btnText);
+  const replyStyle = {
+    width: '24px',
+    height: '24px',
+  };
+  const commentStyle = {
+    width: '40px',
+    height: '40px',
+  };
+  const style = props.comment ? commentStyle : replyStyle;
+
+  return (
+    <section className={classes.AddComment}>
+      <img style={style} alt="avatar" />
+      <input
+        type="text"
+        placeholder="Add a public comment"
+        onFocus={() => setShow(true)}
+      />
+      {show ? (
+        <div>
+          <button onClick={() => setShow(false)}>Cancel</button>
+          <button>{props.comment ? 'Comment' : 'Reply'}</button>
+        </div>
+      ) : null}
+    </section>
+  );
 };
 
-export default addComment;
+export default AddComment;
