206734c349930c598317cf279e63a977accf33db
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-08-10 10:00:42 +0300

    add Description component

--- src/components/Video/Description/Description.js
+++ src/components/Video/Description/Description.js
@@ -1,7 +1,35 @@
 import React from 'react';
-
+import classes from './Description.module.css';
 const description = (props) => {
-  return <section>Description</section>;
+  return (
+    <section className={classes.Description}>
+      <h2>6 Investing Habits That Changed My Life</h2>
+      <div className={classes.InfoMenu}>
+        <p>
+          421, 569 views <small>&sdot;</small> Jan 20, 2020
+        </p>
+        <div>
+          <button>Like</button>
+          <button>Dislike</button>
+          <button>Share</button>
+          <button>Save</button>
+          <button>Options</button>
+        </div>
+      </div>
+      <div className={classes.Info}>
+        <div>
+          <img alt="Photo" />
+          <div>
+            <p>Name</p>
+            <p>2.26M subscribers</p>
+          </div>
+
+          <button>SUBSCRIBE</button>
+        </div>
+        <p>Lorem ipsum a bunch of words</p>
+      </div>
+    </section>
+  );
 };
 
 export default description;
